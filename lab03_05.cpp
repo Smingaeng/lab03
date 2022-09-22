22ca5689adad79860c7a42ead1c9a3102c598e6c
minkyung <minkyung@DESKTOP-RUC9QSR>
2022-09-22 18:32:53 +0900

    초기 커밋.

---
+++ lab03_05.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+	int a = 5;
+	int& x = a;
+	std::cout << x << std::endl;
+}
---
+++ lab03_08.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+	int array[3] = { 8,1,7 };
+	/* 코드 작성 */ // array 배열과 연결되는 레퍼런스 배열 re 정의
+	/* 코드 작성 */ // re에 값 입력
+	int(&re)[3] = array;
+	re[1] = 7;
+	re[2] = 1;
+	std::cout << "Array: " << array[0] << array[1] << array[2] << std::endl;
+	std::cout << "Reference: " << re[0] << re[1] << re[2] << std::endl;
+	return 0;
+}
